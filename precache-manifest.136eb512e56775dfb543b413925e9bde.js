2142a79850bb3cbc7261fd404d31dd2005221bd9
Parth909 <parth.bhoir909@gmail.com>
2020-12-22 20:41:26 +0530

    Updates

---
+++ precache-manifest.136eb512e56775dfb543b413925e9bde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600173fade8ba7c498af7ab769683c74",
+    "url": "/PWA/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/PWA/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "09ce517e0a99fc0bc72a",
+    "url": "/PWA/static/css/main.83b42b3b.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/PWA/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PWA/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ce517e0a99fc0bc72a",
+    "url": "/PWA/static/js/main.825b2907.chunk.js"
+  },
+  {
+    "revision": "7627309718546d5816d3",
+    "url": "/PWA/static/js/runtime-main.dada05b4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
